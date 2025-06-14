bea4840b0131e61cacfc826de4e4ae82c497721a
tharunkumar8899 <tharunkumarpotluri@gmail.com>
2025-06-14 17:02:53 +0530

    (enhancement) added mail link

--- tech.py
+++ tech.py
@@ -20,6 +20,7 @@ def home():
             <p>
                 Generate Your digital posts in just 5 mins!
             </p>
+            <a herf="mailto:tharunkumarpotluri@gmail.com>The work is inprogress</a>
         </body>
 
         </html>
