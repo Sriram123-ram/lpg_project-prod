e5db7c7d83deb3dd661ef366c77f11e35486b144
tharunkumar8899 <tharunkumarpotluri@gmail.com>
2025-06-24 12:36:15 +0530

    Update static assets via package update

--- tech.py
+++ tech.py
@@ -1,12 +1,15 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from supabase import create_client, Client
-from fastapi.middleware.cors import CORSMiddleware
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 
 
 app = FastAPI()
 
-# Supabase credentials
+templates = Jinja2Templates(directory="templates")
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
 supabase_url = "https://dmrmooovmshrxfjlmpgg.supabase.co"
 supabase_api_key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRtcm1vb292bXNocnhmamxtcGdnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk5Njc5MDUsImV4cCI6MjA2NTU0MzkwNX0.3m5dYlHWN8xolLEnWhbAnud0R0QslykMIl-FHsR54XM"
 
@@ -14,24 +17,8 @@ supabase_api_key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSI
 database: Client = create_client(supabase_url, supabase_api_key)
 
 @app.get("/", response_class=HTMLResponse)
-def home():
-    return """
-    <!DOCTYPE html>
-<html>
- 
-<head>
-    <title>LinkedIn Post Generator</title>
-</head>
- 
-<body>
-    <h2>LinkedIn Post Generator</h2>
-    <p>
-        Default code has been
-        loaded into the Editor.
-    </p>
-</body>
-</html>
-"""
+def home(request:Request):
+    return templates.TemplateResponse("index.html", {"request": request})
 
 @app.get("/all-data")
 def get_all_data():
@@ -49,10 +36,3 @@ def get_all_data():
         "preferences": preferences
     }
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
